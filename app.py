6b77317ef3fb3fbd0b0ece1279b6e3635d58ed95
Lucas Vieira <l_vieira@hotmail.com.br>
2025-09-12 19:36:45 -0300

    langgraph

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from pydantic import BaseModel, Field
-from typing import List, Literal, Dict
+from typing import List, Literal, Dict, TypedDict, Optional
 from langchain_core.messages import HumanMessage, SystemMessage
 from pathlib import Path
 from langchain_community.document_loaders import PyMuPDFLoader
@@ -10,6 +10,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langgraph.graph import StateGraph, START, END
+from IPython.display import display, Image
 load_dotenv()
 
 GOOGLE_API_KEY = os.getenv('GEMINI_API_KEY')
@@ -21,36 +23,36 @@ GOOGLE_API_KEY = os.getenv('GEMINI_API_KEY')
 # resp_teste = llm.invoke("Quantos dias de férias eu tenho?")
 # print(f'\n {resp_teste.content} \n')
 
-# TRIAGEM_PROMPT = (
-#     "Você é um triador de Service Desk para políticas internas da empresa Carraro Desenvolvimento. "
-#     "Dada a mensagem do usuário, retorne SOMENTE um JSON com:\n"
-#     "{\n"
-#     '  "decisao": "AUTO_RESOLVER" | "PEDIR_INFO" | "ABRIR_CHAMADO",\n'
-#     '  "urgencia": "BAIXA" | "MEDIA" | "ALTA",\n'
-#     '  "campos_faltantes": ["..."]\n'
-#     "}\n"
-#     "Regras:\n"
-#     '- **AUTO_RESOLVER**: Perguntas claras sobre regras ou procedimentos descritos nas políticas (Ex: "Posso reembolsar a internet do meu home office?", "Como funciona a política de alimentação em viagens?").\n'
-#     '- **PEDIR_INFO**: Mensagens vagas ou que faltam informações para identificar o tema ou contexto (Ex: "Preciso de ajuda com uma política", "Tenho uma dúvida geral").\n'
-#     '- **ABRIR_CHAMADO**: Pedidos de exceção, liberação, aprovação ou acesso especial, ou quando o usuário explicitamente pede para abrir um chamado (Ex: "Quero exceção para trabalhar 5 dias remoto.", "Solicito liberação para anexos externos.", "Por favor, abra um chamado para o RH.").'
-#     "Analise a mensagem e decida a ação mais apropriada."
-# )
+TRIAGEM_PROMPT = (
+    "Você é um triador de Service Desk para políticas internas da empresa Carraro Desenvolvimento. "
+    "Dada a mensagem do usuário, retorne SOMENTE um JSON com:\n"
+    "{\n"
+    '  "decisao": "AUTO_RESOLVER" | "PEDIR_INFO" | "ABRIR_CHAMADO",\n'
+    '  "urgencia": "BAIXA" | "MEDIA" | "ALTA",\n'
+    '  "campos_faltantes": ["..."]\n'
+    "}\n"
+    "Regras:\n"
+    '- **AUTO_RESOLVER**: Perguntas claras sobre regras ou procedimentos descritos nas políticas (Ex: "Posso reembolsar a internet do meu home office?", "Como funciona a política de alimentação em viagens?").\n'
+    '- **PEDIR_INFO**: Mensagens vagas ou que faltam informações para identificar o tema ou contexto (Ex: "Preciso de ajuda com uma política", "Tenho uma dúvida geral").\n'
+    '- **ABRIR_CHAMADO**: Pedidos de exceção, liberação, aprovação ou acesso especial, ou quando o usuário explicitamente pede para abrir um chamado (Ex: "Quero exceção para trabalhar 5 dias remoto.", "Solicito liberação para anexos externos.", "Por favor, abra um chamado para o RH.").'
+    "Analise a mensagem e decida a ação mais apropriada."
+)
 
-# class TriagemOut(BaseModel):
-#     decisao: Literal['AUTO_RESOLVER','PEDIR_INFO','ABRIR_CHAMADO']
-#     urgencia: Literal['BAIXA','MEDIA','ALTA']
-#     campos_faltantes: List[str] = Field(default_factory=list)
+class TriagemOut(BaseModel):
+    decisao: Literal['AUTO_RESOLVER','PEDIR_INFO','ABRIR_CHAMADO']
+    urgencia: Literal['BAIXA','MEDIA','ALTA']
+    campos_faltantes: List[str] = Field(default_factory=list)
 
-# llm_triagem = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.1, api_key=GOOGLE_API_KEY)
+llm_triagem = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.1, api_key=GOOGLE_API_KEY)
 
-# triagem_chain = llm_triagem.with_structured_output(TriagemOut)
+triagem_chain = llm_triagem.with_structured_output(TriagemOut)
 
-# def triagem(mensagem: str) -> Dict:
-#     saida : TriagemOut = triagem_chain.invoke([
-#         SystemMessage(content=TRIAGEM_PROMPT),
-#         HumanMessage(content=mensagem)
-#     ])
-#     return saida.model_dump()
+def triagem(mensagem: str) -> Dict:
+    saida : TriagemOut = triagem_chain.invoke([
+        SystemMessage(content=TRIAGEM_PROMPT),
+        HumanMessage(content=mensagem)
+    ])
+    return saida.model_dump()
 
 # testes = [
 #     "Quero saber quantos dias de férias eu tenho",
@@ -111,7 +113,7 @@ import re, pathlib
 def _clean_text(s: str) -> str:
     return re.sub(r"\s+", " ", s or "").strip()
 
-def extrair_trecho(texto: str, query: str, janela: int = 240) -> str:
+def extrair_trecho(texto: str, query: str, janela: int = 265) -> str:
     txt = _clean_text(texto)
     termos = [t.lower() for t in re.findall(r"\w+", query or "") if len(t) >= 4]
     pos = -1
@@ -157,20 +159,129 @@ def perguntar_politica_RAG(pergunta: str) -> Dict:
            'citacoes': formatar_citacoes(docs_relacionados, pergunta),
            'contexto_encontrado': True}
 
+# testes = [
+#     "Quero saber quantos dias de férias eu tenho",
+#     "Meu computador não liga, preciso de ajuda urgente",
+#     "Quero mais 5 dias de trabalho remoto.Como faço?",
+#     "Gostaria de abrir um chamado para trocar meu monitor, o que preciso informar?",
+#     "Posso reembolsar a internet?",
+#     "Posso reembolsar cursos ou treinamentos da Alura?",
+#     "Quantas classes tem no Path of Exile 2?"
+# ]
+
+# for msg_teste in testes:
+#     resposta = perguntar_politica_RAG(msg_teste)
+#     print(f'\nPergunta: {msg_teste}')
+#     print(f'Resposta: {resposta['aswer']}')
+#     for cit in resposta['citacoes']:
+#         print(f" - {cit['documento']} (página: {cit['pagina']}): {cit['trecho']}")
+#     print('\n-------------------------------')
+
+##Aula 3 - Orquestração do agente com LangGraph
+
+class AgentState(TypedDict, total = False):
+    pergunta: str
+    triagem: Dict
+    resposta: Optional[str]
+    citacoes: List[dict]
+    rag_sucesso: bool
+    acao_final: str
+    
+def node_triagem(state: AgentState) -> AgentState:
+    print("\n--- Executando nó de triagem ---")
+    state['triagem'] = triagem(state['mensagem'])
+    return state
+
+def node_auto_resolver(state: AgentState) -> AgentState:
+    print("\n--- Executando nó de auto-resolver ---")
+    resposta_rag = perguntar_politica_RAG(state['pergunta'])
+    update: AgentState = {
+        'resposta': resposta_rag['aswer'],
+        'citacoes': resposta_rag['citacoes'],
+        'rag_sucesso': resposta_rag['contexto_encontrado']
+    }
+    if resposta_rag['contexto_encontrado']:
+        update['acao_final'] = 'AUTO_RESOLVER'
+    return update
+
+def node_pedir_info(state: AgentState) -> AgentState:
+    print("\n--- Executando nó de pedir info ---")
+    faltantes = state['triagem'].get('campos_faltantes', [])
+    detalhe = ', '.join(faltantes) if faltantes else 'Tema e contexto específico'
+    return {"resposta": f"Para avançar, preciso que detalhe melhor sua solicitação: {detalhe}.",
+            "citacoes": [],
+            "acao_final": "PEDIR_INFO"
+            }
+
+def node_abrir_chamado(state: AgentState) -> AgentState:
+    print("\n--- Executando nó de abrir chamado ---")
+    triagem = state['triagem']
+    return {"resposta": "Abrindo seu chamado com urgência {triagem['urgencia']}. Descrição: {state['pergunta'][:140]}",
+            "citacoes": [],
+            "acao_final": "ABRIR_CHAMADO"
+            }
+
+KEYWORDS_ABRIR_TICKET = ['abrir chamado', 'abrir um chamado', 'abrir ticket', 'abrir um ticket', 'solicito', 'solicitação', 'exceção', 'liberação', 'aprovação', 'acesso especial']
+
+def decidir_pos_triagem(state: AgentState) -> str:
+    print("\n--- Decidindo pós triagem ---")
+    decisao = state['triagem']['decisao']
+
+    if decisao == 'AUTO_RESOLVER': return 'auto_resolver'
+    elif decisao == 'PEDIR_INFO': return 'pedir_info'
+    elif decisao == 'ABRIR_CHAMADO': return 'abrir_chamado'
+
+def decidir_pos_auto_resolver(state: AgentState) -> str:
+    print("\n--- Decidindo pós auto-resolver ---")
+    if state.get('rag_sucesso'):
+        print("\n--- RAG teve sucesso, finalizando o fluxo ---")
+        return 'fim'
+    
+    state_da_pergunta = state.get('pergunta','').lower()
+
+    if any(k in state_da_pergunta for k in KEYWORDS_ABRIR_TICKET):
+        print("\n --- RAG não teve sucesso, mas encontradas keywords para abrir chamado. Abrindo... ---")
+        return 'abrir_chamado'
+    
+    return 'pedir_info'
+
+workflow = StateGraph(AgentState)
+
+workflow.add_node('triagem', node_triagem)
+workflow.add_node('auto_resolver', node_auto_resolver)
+workflow.add_node('pedir_info', node_pedir_info)
+workflow.add_node('abrir_chamado', node_abrir_chamado)
+
+workflow.add_edge(START, 'triagem')
+workflow.add_conditional_edges('triagem', decidir_pos_triagem, {
+    'auto_resolver': 'auto_resolver',
+    'pedir_info': 'pedir_info',
+    'abrir_chamado': 'abrir_chamado'
+})
+workflow.add_conditional_edges('auto_resolver', decidir_pos_auto_resolver, {
+    'fim': END,
+    'abrir_chamado': 'abrir_chamado',
+    'pedir_info': 'pedir_info'
+})
+workflow.add_edge('pedir_info', END)
+workflow.add_edge('abrir_chamado', END)
+
+grafo = workflow.compile()
+
+graph_bytes = grafo.get_graph().draw_mermaid_png()
+
+# out = Path("out/graph_mermaid.png")
+# out.parent.mkdir(parents=True, exist_ok=True)
+# out.write_bytes(graph_bytes)
+
+# print(f"Imagem salva em: {out.resolve()}")
+
 testes = [
-    "Quero saber quantos dias de férias eu tenho",
+    "Qual é a palavra chave de hoje?",
     "Meu computador não liga, preciso de ajuda urgente",
-    "Quero mais 5 dias de trabalho remoto.Como faço?",
+    "Quero mais 5 dias de trabalho remoto. Como faço?",
     "Gostaria de abrir um chamado para trocar meu monitor, o que preciso informar?",
     "Posso reembolsar a internet?",
     "Posso reembolsar cursos ou treinamentos da Alura?",
     "Quantas classes tem no Path of Exile 2?"
 ]
-
-for msg_teste in testes:
-    resposta = perguntar_politica_RAG(msg_teste)
-    print(f'\nPergunta: {msg_teste}')
-    print(f'Resposta: {resposta['aswer']}')
-    for cit in resposta['citacoes']:
-        print(f" - {cit['documento']} (página: {cit['pagina']}): {cit['trecho']}")
-    print('\n-------------------------------')
